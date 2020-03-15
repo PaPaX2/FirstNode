4f3203725f4d9174a7bcf90cd13b12407343bfb6
Kajetan Terlecki <k.terlecki@wp.pl>
2020-03-15 14:58:56 +0100

    IMPROVEMENT code

--- App.js
+++ App.js
@@ -16,25 +16,24 @@ randChoice = (arr) => {
 const people = [];
 
 for (let i = 1; i <= 20; i++ ) {
-const gender = randChoice(genders);
-let name = '';
-const lastName = randChoice(lastNames);
+  const gender = randChoice(genders);
+  const lastName = randChoice(lastNames);
+  let name = '';
 
-if (gender === 'Male') {
+  if (gender === 'Male') {
     name = randChoice(maleNames);
   }
   else {name = randChoice(femaleNames);
   }
+
   const minAge = 18;
   const maxAge = 78;
   const age = Math.floor(Math.random()* (maxAge - minAge) + minAge);
-  
  
-  people.push({geder: gender, name: name, lastName: lastName, age: age});
+  people.push({gender: gender, name: name, lastName: lastName, age: age});
 };
-console.log('people:', people);
 
 fs.writeFile('people.json', JSON.stringify(people), (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-  });
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
